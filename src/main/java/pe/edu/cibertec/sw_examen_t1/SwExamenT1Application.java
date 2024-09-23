07b80a048c0845e2414f75d880440190dd06dce5
Natha23XO <hugocastroleon2301@gmail.com>
2024-09-23 18:25:31 -0500

    Subiendo a repositorio

---
+++ src/main/java/pe/edu/cibertec/sw_examen_t1/SwExamenT1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.sw_examen_t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwExamenT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwExamenT1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/sw_examen_t1/SwExamenT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.sw_examen_t1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwExamenT1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
