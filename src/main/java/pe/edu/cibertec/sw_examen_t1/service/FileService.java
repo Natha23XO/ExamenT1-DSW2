4f0846ab040e3c22e8feed38b4653e05c1273a74
Genesis <genesis@kaztro.cloud>
2024-09-23 19:16:42 -0500

    cargando metodos en file service

--- src/main/java/pe/edu/cibertec/sw_examen_t1/service/FileService.java
+++ src/main/java/pe/edu/cibertec/sw_examen_t1/service/FileService.java
@@ -1,5 +1,29 @@
 package pe.edu.cibertec.sw_examen_t1.service;
 
-public class FileService {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Slf4j
+    @Service
+    public class FileService {
+
+    private static final String FILE_PATH = "archivo1.txt";
+    private static final String XML_FILE_PATH = "archivo2.xml";
+    private static final String JSON_FILE_PATH = "archivo3.json";
+
+
+    public void crearArchivoTxt() throws IOException {
+
+    }
+
+    public void crearArchivoJson() throws Exception {
+
+
+}
+    public void crearArchivoXML() throws Exception {
+
+    }
 
 }
