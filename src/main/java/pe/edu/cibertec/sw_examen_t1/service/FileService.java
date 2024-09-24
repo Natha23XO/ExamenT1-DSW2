940d132a584dd8f59fbc1ea132b12f4d02522f16
PatrickMontes <alexandermontes30p@hotmail.com>
2024-09-23 19:32:50 -0500

    método "crearArchivoTxt" listo

--- src/main/java/pe/edu/cibertec/sw_examen_t1/service/FileService.java
+++ src/main/java/pe/edu/cibertec/sw_examen_t1/service/FileService.java
@@ -2,12 +2,16 @@ package pe.edu.cibertec.sw_examen_t1.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import pe.edu.cibertec.sw_examen_t1.model.Animal;
 
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Slf4j
-    @Service
-    public class FileService {
+@Service
+public class FileService {
 
     private static final String FILE_PATH = "archivo1.txt";
     private static final String XML_FILE_PATH = "archivo2.xml";
@@ -15,13 +19,24 @@ import java.io.IOException;
 
 
     public void crearArchivoTxt() throws IOException {
+        Animal animal = new Animal();
 
+        try(FileOutputStream fos = new FileOutputStream(FILE_PATH);
+            DataOutputStream dos = new DataOutputStream(fos)) {
+
+            dos.writeUTF(animal.getNombre());
+            dos.writeInt(animal.getEdad());
+            dos.writeUTF(animal.getTipo());
+
+        }catch (IOException e){
+            log.error(e.getMessage());
+        }
     }
 
     public void crearArchivoJson() throws Exception {
 
 
-}
+    }
     public void crearArchivoXML() throws Exception {
 
     }
